ab41859e673c0253164b95ad0e22a460c82a86d7
Jesse Granier <jesse.granier@lapiscine.pro>
2024-01-30 14:24:37 +0100

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+import Connexion from './pages/Connexion';
+import Contact from './pages/Contact';
+import Mentions from './pages/Mentionslegales';
+import Recherche from './pages/Recherche';
+import Services from './pages/Services';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <title>La Bibliothèque d'Hyrule</title>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/login" element={<Connexion />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/mentions" element={<Mentions />} />
+        <Route path="/search" element={<Recherche />} />
+        <Route path="/services" element={<Services />} />      
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/user/Header';
+import Footer from '../components/user/Footer';
+
+
+const HomePage = () => {
+  return (
+    <body>
+      <main>
+        <Header />
+        <p>Test de page</p>
+        <Footer />
+        </main>
+    </body>
+  );
+};
+
+export default HomePage;
